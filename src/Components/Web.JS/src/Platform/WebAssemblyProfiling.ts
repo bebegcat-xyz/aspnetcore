fa807020ea7c978c5bdce9076a1602e6b845a62b
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-06-29 16:56:50 +0100

    Clean up capture infrastructure

--- src/Components/Web.JS/src/Platform/WebAssemblyProfiling.ts
+++ src/Components/Web.JS/src/Platform/WebAssemblyProfiling.ts
@@ -1,11 +1,11 @@
 import { System_String } from "./Platform";
 
-interface TimingRegion {
+interface TimingEntry {
     // To minimize overhead, don't even decode the strings that arrive from .NET. Assume they are compile-time constants
     // and hence the memory address will be fixed, so we can just store the pointer value.
     name: string | System_String;
-    startTime: number;
-    endTime?: number;
+    type: 'start' | 'end';
+    timestamp: number;
 }
 
 interface TraceEvent {
@@ -19,57 +19,46 @@ interface TraceEvent {
 }
 
 let _captureStartTime = 0;
-const _openRegionsStack: TimingRegion[] = [];
-const _startLog: TimingRegion[] = [];
-const _endLog: TimingRegion[] = [];
+const _entryLog: TimingEntry[] = [];
+const _openRegionsStack: (string | System_String)[] = [];
 
 export function profileStart(name: System_String | string) {
+    const startTime = performance.now();
     if (!_captureStartTime) {
-        _captureStartTime = performance.now();
+        _captureStartTime = startTime;
     }
 
-    const region = { name: name, startTime: performance.now() };
-    _openRegionsStack.push(region);
-    _startLog.push(region);
+    _openRegionsStack.push(name);
+    _entryLog.push({ name: name, type: 'start', timestamp: startTime });
 }
 
 export function profileEnd(name: System_String | string) {
     const endTime = performance.now();
-    const poppedRegion = _openRegionsStack.pop();
-    if (!poppedRegion) {
+    const poppedRegionName = _openRegionsStack.pop();
+    if (!poppedRegionName) {
         throw new Error(`Profiling mismatch: tried to end profiling for '${readJsString(name)}', but the stack was empty.`);
-    } else if (poppedRegion.name !== name) {
-        throw new Error(`Profiling mismatch: tried to end profiling for '${readJsString(name)}', but the top stack item was '${readJsString(poppedRegion.name)}'.`);
+    } else if (poppedRegionName !== name) {
+        throw new Error(`Profiling mismatch: tried to end profiling for '${readJsString(name)}', but the top stack item was '${readJsString(poppedRegionName)}'.`);
     }
 
-    poppedRegion.endTime = endTime;
-    _endLog.push(poppedRegion);
+    _entryLog.push({ name: name, type: 'end', timestamp: endTime });
 }
 
 export function profileReset() {
     _openRegionsStack.length = 0;
-    _startLog.length = 0;
-    _endLog.length = 0;
+    _entryLog.length = 0;
     _captureStartTime = 0;
 }
 
 export function profileExport() {
-    // Merge the two logs into a single ordered list of trace events
-    const traceEvents: TraceEvent[] = [];
-    let startLogIndex = 0, endLogIndex = 0;
-    while (startLogIndex < _startLog.length || endLogIndex < _endLog.length) {
-        const nextStart = _startLog[startLogIndex];
-        const nextEnd = _endLog[endLogIndex];
-        const useStart = !nextEnd || (nextStart && nextStart.startTime <= nextEnd.endTime!);
-        if (useStart) {
-            traceEvents.push({ name: readJsString(nextStart.name)!, cat: 'PERF', ph: 'B', ts: (nextStart.startTime - _captureStartTime) * 1000, pid: 0, tid: 0 });
-            startLogIndex++;
-        } else {
-            traceEvents.push({ name: readJsString(nextEnd.name)!, cat: 'PERF', ph: 'E', ts: (nextEnd.endTime! - _captureStartTime) * 1000, pid: 0, tid: 0 });
-            endLogIndex++;
-        }
-    }
-
+    const traceEvents: TraceEvent[] = _entryLog.map(entry => ({
+        name: readJsString(entry.name)!,
+        cat: 'PERF',
+        ph: entry.type === 'start' ? 'B': 'E',
+        ts: (entry.timestamp - _captureStartTime) * 1000,
+        pid: 0,
+        tid: 0,
+    }));
     const traceEventsJson = JSON.stringify(traceEvents);
     const traceEventsBuffer = new TextEncoder().encode(traceEventsJson);
     const anchorElement = document.createElement('a');
